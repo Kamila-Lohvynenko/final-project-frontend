bba52fd197c3335188ab9c2c44ba0422b67cd9bb
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-09-12 19:26:10 +0300

    Add WaterMainInfo component structure

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,14 +1,30 @@
-const WaterMainInfo = ({ openWaterModal, waterModalState }) => {
-  return (
-    <div>
-      WaterMainInfo
-      <button
-        onClick={() => openWaterModal({ isOpen: true, operation: 'add' })}
-      >
-        open
-      </button>
+// const WaterMainInfo = ({ openWaterModal, waterModalState }) => {
+//   return (
+//     <div>
+//       WaterMainInfo
+//       <button
+//         onClick={() => openWaterModal({ isOpen: true, operation: 'add' })}
+//       >
+//         open
+//       </button>
+//     </div>
+//   );
+// };
+
+import Logo from '../Logo/Logo.jsx';
+import WaterProgressBar from '../WaterProgressBar/WaterProgressBar.jsx';
+import AddWaterBtn from '../AddWaterBtn/AddWaterBtn.jsx';
+import css from '../WaterMainInfo/WaterMainInfo.module.css';
+
+
+const WaterMainInfo =()=>{
+  return(
+    <div className={css.waterContainer}>
+      <Logo />
+      <WaterProgressBar />
+      <AddWaterBtn />
     </div>
-  );
+  )
 };
 
 export default WaterMainInfo;
