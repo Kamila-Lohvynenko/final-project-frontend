ae45aad551b5c37c34e9ce5d26d57ccf743489c3
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-09-13 14:44:14 +0200

    top-bar visuals implemented
    meaning ChooseDate and AddWaterBtn components of DailyInfo

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,9 +1,16 @@
 import css from '../AddWaterBtn/AddWaterBtn.module.css';
 
-const AddWaterBtn =()=>{
-    return(
+const AddWaterBtn = ({inDetails}) => {
+    return( !inDetails ?
         <button type="button" className={css.btnAdd}>
             <h2 className={css.btnText}>
+            Add water
+            </h2>            
+        </button>
+        : 
+        <button type="button" className={css.detailsBtnAdd}>
+            <div className={css.detailsAddSign}>+</div> {/* replace with the actual icon */}
+            <h2 className={css.detailsBtnText}>
             Add water
             </h2>            
         </button>
--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,5 +1,17 @@
+import css from "./DailyInfo.module.css";
+import ChooseDate from "../ChooseDate/ChooseDate";
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
+
 const DailyInfo = () => {
-  return <div>DailyInfo</div>;
+  return (
+    <div>
+      <div className={css.topContainer}>
+        <ChooseDate/>
+        <AddWaterBtn inDetails={true} />
+      </div>
+
+    </div>
+  );
 };
 
 export default DailyInfo;
