192d8f3ed657484f876749a5de73e2b8fe7233d5
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-09-13 22:17:02 +0300

    Update WaterModal

--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,38 +1,3 @@
-// const WaterModal = ({ onClose }) => {
-//   const handleClose = () => {
-//     onClose("waterModal");
-//   };
-//   return (
-//     <div>
-//       WaterModal
-//       <button type="button" onClick={handleClose}>
-//         Close
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default WaterModal;
-
-// import WaterForm from "../WaterForm/WaterForm.jsx";
-
-// const WaterModal = ({ onClose }) => {
-//   const handleClose = () => {
-//     onClose();
-//   };
-//   return (
-//     <div>      
-//       <WaterForm/>
-//       <button type="button" onClick={handleClose}>
-//         Close
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default WaterModal;
-
-
 import WaterForm from "../WaterForm/WaterForm.jsx";
 
 const WaterModal = ({ onClose }) => {
