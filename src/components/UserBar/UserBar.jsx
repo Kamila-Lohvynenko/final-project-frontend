521bb4a7400f19506d55807e31d32bcb4fd7a86e
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-09-14 21:50:10 +0200

    correct

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,35 +1,46 @@
-import { useState, useRef } from "react";
+import { useState, useRef } from 'react';
 import UserBarPopover from '../UserBarPopover/UserBarPopover';
 import css from './UserBar.module.css';
-import { FaChevronDown, FaChevronUp  } from 'react-icons/fa';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
-const UserBar = ({ user }) => {
-    const [isPopoverOpen, setPopoverOpen] = useState(false);
-    const buttonRef = useRef(null); 
+const UserBar = ({ user, setSettingsModal, setLogoutModal }) => {
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
+  const buttonRef = useRef(null);
 
-    const togglePopover = () => {
-        setPopoverOpen(!isPopoverOpen);
-    };
+  const togglePopover = () => {
+    setPopoverOpen(!isPopoverOpen);
+  };
 
-    return (
-        <div className={css.userBar}>
-            <button 
-                onClick={togglePopover} 
-                className={`${css.userBarButton} ${isPopoverOpen ? css.open : ''}`}
-                ref={buttonRef} 
-            >
-                <span>{user?.name || "Guest"} </span>
-                <img src={user?.avatar || 'default-avatar.png'} alt="User Avatar" className={css.userAvatar} />
-                
-                {isPopoverOpen ? (
-                    <FaChevronUp className={css.arrow} />
-                ) : (
-                    <FaChevronDown className={css.arrow} />
-                )}
-            </button>
-            {isPopoverOpen && <UserBarPopover onClose={togglePopover} buttonRef={buttonRef} />}
-        </div>
-    );
+  return (
+    <div className={css.userBar}>
+      <button
+        onClick={togglePopover}
+        className={`${css.userBarButton} ${isPopoverOpen ? css.open : ''}`}
+        ref={buttonRef}
+      >
+        <span>{user?.name || 'Guest'} </span>
+        <img
+          src={user?.avatar || 'default-avatar.png'}
+          alt="User Avatar"
+          className={css.userAvatar}
+        />
+
+        {isPopoverOpen ? (
+          <FaChevronUp className={css.arrow} />
+        ) : (
+          <FaChevronDown className={css.arrow} />
+        )}
+      </button>
+      {isPopoverOpen && (
+        <UserBarPopover
+          onClose={togglePopover}
+          buttonRef={buttonRef}
+          setSettingsModal={setSettingsModal}
+          setLogoutModal={setLogoutModal}
+        />
+      )}
+    </div>
+  );
 };
 
 export default UserBar;
