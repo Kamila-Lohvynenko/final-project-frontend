e75fe1a62ec03a26c64108a640ed39126a84bb83
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-09-15 00:06:04 +0300

    reack-form

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -4,6 +4,8 @@ import * as Yup from 'yup';
 import styles from './SignInForm.module.css';
 import sprite from '../../images/sprite.svg';
 import Logo from '../Logo/Logo';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().email('Must be a valid email').required('Required'),
@@ -15,6 +17,24 @@ const SignInForm = () => {
   const passwordId = useId();
   const [visiblePassword, setVisiblePassword] = useState(false);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    trigger,
+  } = useForm({
+    values: { email: '', password: '' },
+    resolver: yupResolver(validationSchema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = ({ email, password }) => {
+    console.log({
+      email,
+      password,
+    });
+  };
   return (
     <>
       <div className={styles.logo}>
@@ -22,17 +42,27 @@ const SignInForm = () => {
       </div>
       <div className={styles.wrapperSignIn}>
         <h2 className={styles.title}>Sign In</h2>
-        <form className={styles.form}>
+        <form
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmit)}
+          className={styles.form}
+        >
           <div className={styles.field}>
             <label htmlFor={emailId} className={styles.label}>
               Email
             </label>
             <input
               type="email"
+              {...register('email')}
               id={emailId}
               placeholder="Enter your email"
-              className={styles.input}
+              className={`${styles.input} ${errors.email ? styles.error : ''}`}
+              onBlur={() => trigger('email')}
             />
+            {errors.email && (
+              <p className={styles.errorMessage}>{errors.email.message}</p>
+            )}
           </div>
 
           <div className={styles.field}>
@@ -42,9 +72,13 @@ const SignInForm = () => {
             <div className={styles.wrapper_icon}>
               <input
                 type={visiblePassword ? 'text' : 'password'}
+                {...register('password')}
                 id={passwordId}
                 placeholder="Enter your password"
-                className={styles.input}
+                className={`${styles.input} ${
+                  errors.password ? styles.error : ''
+                }`}
+                onBlur={() => trigger('password')}
               />
               <svg
                 className={styles.icon_eye}
@@ -58,6 +92,9 @@ const SignInForm = () => {
                   }`}
                 />
               </svg>
+              {errors.password && (
+                <p className={styles.errorMessage}>{errors.password.message}</p>
+              )}
             </div>
           </div>
           <button type="submit" className={styles.btn}>
