3f88641e1f3fc5e0086ac1cab6d5543712e92924
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-09-14 15:00:23 +0200

    n

---
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -0,0 +1,60 @@
+import { useRef, useState, useEffect } from "react";
+import { usePopper } from 'react-popper';
+import UserSettingsModal from '../UserSettingsModal/UserSettingsModal';
+import LogOutModal from '../LogOutModal/LogOutModal';
+import css from './UserBarPopover.module.css';
+import { FaCog, FaSignOutAlt } from 'react-icons/fa';
+
+const UserBarPopover = ({ onClose, buttonRef }) => { 
+    const [isSettingsOpen, setSettingsOpen] = useState(false);
+    const [isLogOutOpen, setLogOutOpen] = useState(false);
+    
+    const popperRef = useRef(null); 
+    const { styles, attributes } = usePopper(buttonRef.current, popperRef.current, {
+        placement: 'bottom-start',
+        modifiers: [
+            {
+                name: 'flip',
+                options: {
+                    fallbackPlacements: ['bottom-end', 'top-start', 'top-end'],
+                },
+            },
+            {
+                name: 'preventOverflow',
+                options: {
+                    padding: 8,
+                },
+            },
+        ],
+    });
+
+    const handleClickOutside = (event) => {
+        if (popperRef.current && !popperRef.current.contains(event.target)) {
+            onClose();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <>
+            <div ref={popperRef} style={styles.popper} {...attributes.popper} className={css.userBarPopover}>
+                <button onClick={() => setSettingsOpen(true)}>
+                    <FaCog className={css.popoverIcon} />Settings
+                </button>
+                <button onClick={() => setLogOutOpen(true)}>
+                    <FaSignOutAlt className={css.popoverIcon} />Log out
+                </button>
+            </div>
+            {isSettingsOpen && <UserSettingsModal onClose={() => setSettingsOpen(false)} />}
+            {isLogOutOpen && <LogOutModal onClose={() => setLogOutOpen(false)} />}
+        </>
+    );
+};
+
+export default UserBarPopover;
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,13 @@
-const UserPanel = () => {
-  return <div>UserPanel</div>;
+import UserBar from '../UserBar/UserBar';
+import css from './UserPanel.module.css';
+
+const UserPanel = ({ user }) => {
+  return (
+    <div className={css.userPanel}>
+      <h2>Hello, {user?.name || "Guest"}!</h2> 
+      <UserBar userName={user?.name || "Guest"} /> 
+    </div>
+  );
 };
 
 export default UserPanel;
