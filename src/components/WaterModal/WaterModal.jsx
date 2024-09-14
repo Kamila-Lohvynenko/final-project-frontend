751b0dd78d9b28999ff34670253acb0cd05a5cf5
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-09-15 01:27:32 +0300

    Added constants in WaterModal and WaterForm

--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,10 +1,25 @@
+// import WaterForm from "../WaterForm/WaterForm.jsx";
+// import css from '../WaterModal/WaterModal.module.css';
+
+// const WaterModal = ({operationType = "add"}) => {
+//   return (
+//     <div className={css.waterModal}>
+//       <h1>{operationType === "add" ? "Add water" : "Edit the entered amount of water"} </h1>           
+//       <WaterForm/>
+//     </div>
+//   );
+// };
+
+// export default WaterModal;
+
 import WaterForm from "../WaterForm/WaterForm.jsx";
 import css from '../WaterModal/WaterModal.module.css';
+import {OPERATION_NAME} from '../../constants';
 
-const WaterModal = ({operationType = "add"}) => {
+const WaterModal = ({operationType = OPERATION_NAME.ADD_WATER}) => {
   return (
     <div className={css.waterModal}>
-      <h1>{operationType === "add" ? "Add water" : "Edit the entered amount of water"} </h1>           
+      <h1>{operationType === OPERATION_NAME.ADD_WATER ? "Add water" : "Edit the entered amount of water"} </h1>           
       <WaterForm/>
     </div>
   );
