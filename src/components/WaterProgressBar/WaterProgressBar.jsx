c11eaa014badefc3827b97b4b88166d9a15f1ea6
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-09-14 17:11:02 +0300

    Added slider in WaterProgressBar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,12 +1,28 @@
 import css from '../WaterProgressBar/WaterProgressBar.module.css';
 
 const WaterProgressBar =()=>{
+    const currentWater = 1000;
+    const dailyWaterGoal =2000;
+    const waterIntakeProgress = Math.round((currentWater/dailyWaterGoal) * 100);
+
+    const displayedPercentage = waterIntakeProgress >= 100 ? 100 : waterIntakeProgress;
+
     return(
         <div className={css.progressBarContainer}>
             <h2 className={css.progressBarTitle}>Today</h2>
             <div className={css.progressBar}>
-                <div className={css.progressBarFill}></div>
-                <p className={css.percentNumber}></p>
+                <div className={css.progressBarFill} 
+                style={{width: `${displayedPercentage}%`, 
+                backgroundColor: displayedPercentage >= 100 ? "#7fffd4" : "#9be1a0",
+                }}>                    
+                </div>
+                <div className={css.slider} 
+                style={{
+                    left: `${displayedPercentage}%`,
+                    border: displayedPercentage >= 100 ? "solid 1px #7fffd4" : "solid 1px #9be1a0",
+                    transform: `translate(-50%, -50%)`,
+                }}>                    
+                </div>
             </div>
             <div className={css.percentContainer}>
                 <p className={css.percent}>0%</p>
@@ -16,5 +32,6 @@ const WaterProgressBar =()=>{
         </div>
     );
 };
+
 
 export default WaterProgressBar;
