4285ab0f25a7ecec7743f98abf29c63ced5d26ad
Arsen Akopyan <tigrikpictures@gmail.com>
2024-09-14 20:41:36 +0300

    validation done

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,36 +1,77 @@
 import { useId, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 import css from './SignUpForm.module.css';
 import sprite from '../../images/sprite.svg';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useDispatch } from 'react-redux';
 
-// const validationSchema = Yup.object().shape({
-//   email: Yup.string().email('Must be a valid email').required('Required'),
-//   password: Yup.string().min(10, 'Too short!').required('Required'),
-// });
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Must be a valid email').required('Required'),
+  password: Yup.string().min(5, 'Too short!').required('Required'),
+  repeatPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Please confirm your password'),
+});
 
 const SignUpForm = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    trigger,
+  } = useForm({
+    values: { email: '', password: '', repeatPassword: '' },
+    resolver: yupResolver(validationSchema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (values) => {
+    dispatch(
+      register({
+        email: values.email,
+        password: values.password,
+      }),
+    )
+      .unwrap()
+      .then(() => {
+        reset();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
   const emailId = useId();
   const passwordId = useId();
   const repeatPasswordId = useId();
 
   const [visiblePassword, setVisiblePassword] = useState(false);
   const [visibleRepeatPassword, setVisibleRepeatPassword] = useState(false);
+
   return (
     <>
       <div className={css.wrapper}>
         <h2 className={css.title}>Sign Up</h2>
-        <form>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
           <div className={css.field}>
             <label htmlFor={emailId} className={css.label}>
               Email
             </label>
             <input
               type="email"
+              {...register('email')}
               id={emailId}
               placeholder="Enter your email"
-              className={css.input}
+              className={`${css.input} ${errors.email ? css.error : ''}`}
+              onBlur={() => trigger('email')}
             />
+            {errors.email && (
+              <p className={css.errorMessage}>{errors.email.message}</p>
+            )}
           </div>
 
           <div className={css.field}>
@@ -40,9 +81,11 @@ const SignUpForm = () => {
             <div className={css.wrapperIcon}>
               <input
                 type={visiblePassword ? 'text' : 'password'}
+                {...register('password')}
                 id={passwordId}
                 placeholder="Enter your password"
-                className={css.input}
+                className={`${css.input} ${errors.password ? css.error : ''}`}
+                onBlur={() => trigger('password')}
               />
               <svg
                 className={css.icon}
@@ -57,6 +100,9 @@ const SignUpForm = () => {
                 />
               </svg>
             </div>
+            {errors.password && (
+              <p className={css.errorMessage}>{errors.password.message}</p>
+            )}
           </div>
           <div className={css.field}>
             <label htmlFor={repeatPasswordId} className={css.label}>
@@ -65,9 +111,13 @@ const SignUpForm = () => {
             <div className={css.wrapperIcon}>
               <input
                 type={visibleRepeatPassword ? 'text' : 'password'}
+                {...register('repeatPassword')}
                 id={repeatPasswordId}
                 placeholder="Repeat password"
-                className={css.input}
+                className={`${css.input} ${
+                  errors.repeatPassword ? css.error : ''
+                }`}
+                onBlur={() => trigger('repeatPassword')}
               />
               <svg
                 className={css.icon}
@@ -82,6 +132,11 @@ const SignUpForm = () => {
                 />
               </svg>
             </div>
+            {errors.repeatPassword && (
+              <p className={css.errorMessage}>
+                {errors.repeatPassword.message}
+              </p>
+            )}
           </div>
           <button type="submit" className={css.btn}>
             Sign Up
