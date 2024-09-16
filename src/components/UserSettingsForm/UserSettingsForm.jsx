b50c48b27aea78cc6cca4725525ee4a2703f37e0
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-09-16 16:08:25 +0100

    styles added to the span text in userSettingsForm

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -157,11 +157,11 @@ const UserSettingsForm = () => {
           <p className={`${css.text} ${css.formula} ${css.smallGap}`}>
             V=(M*0,04) + (T*0,6)
           </p>
-          <p className={css.spanText}>
-            * V is the volume of the water norm in liters per day, M is your
-            body weight, T is the time of active sports, or another type of
-            activity commensurate in terms of loads (in the absence of these,
-            you must set 0)
+          <p className={`${css.spanText} ${css.text} ${css.smallGap}`}>
+            <span className={css.specialSign}>*</span> V is the volume of the
+            water norm in liters per day, M is your body weight, T is the time
+            of active sports, or another type of activity commensurate in terms
+            of loads (in the absence of these, you must set 0)
           </p>
           <span>
             <svg className={css.iconExclamationMark}>
