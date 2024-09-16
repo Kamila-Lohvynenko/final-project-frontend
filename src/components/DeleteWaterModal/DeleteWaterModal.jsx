d964e435710e789160a7864a1911c68ef12012d3
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-09-16 13:32:02 +0200

    Delete modal implemented

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { MODAL_NAME } from '../../constants';
+import css from './DeleteWaterModal.module.css'
 
-const DeleteWaterModal = () => {
-  return <div>DeleteWaterModal</div>;
+const DeleteWaterModal = ({ onClose }) => {
+  
+  const handleModalClose = () => {
+    onClose(MODAL_NAME.DELETE_WATER_MODAL);
+  }
+
+  return (
+    <div className={css.deleteContainer}>
+      <div className={css.deleteDescription}>
+        <h3>Delete entry</h3>
+        <p>Are you sure you want to delete the entry?</p>
+      </div>
+      <div className={css.deleteButtons}>
+        <button className={`${css.button} ${css.deleteBtn}`} onClick={handleModalClose}>Delete</button>
+        <button className={`${css.button} ${css.cancelBtn}`}>Cancel</button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteWaterModal;
