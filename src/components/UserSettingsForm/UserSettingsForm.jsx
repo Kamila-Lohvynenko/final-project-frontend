65e7558567796d1e30aa96c5c08ee8ab83d77239
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-09-18 14:38:50 +0100

    styles for desktop - done (all 3 devices stylization done)

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -79,7 +79,6 @@ const UserSettingsForm = () => {
             accept="image/*"
             {...register('avatar')}
             onChange={handleAvatarChange}
-            onBlur={() => trigger('avatar')}
           />
           <label className={`${css.uploadLabel} ${css.text}`} htmlFor="avatar">
             Upload a photo
@@ -91,170 +90,177 @@ const UserSettingsForm = () => {
       </div>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className={css.formContent}>
-            <div className={css.bigGap}>
-              <p className={css.boldText}>Your gender identity</p>
-              <div className={css.radioContainer}>
-                <input
-                  type="radio"
-                  id="woman"
-                  value="woman"
-                  {...register('gender')}
-                  defaultChecked
-                />
+          <div className={css.content}>
+            <div className={css.leftSide}>
+              <div className={css.bigGap}>
+                <p className={css.boldText}>Your gender identity</p>
+                <div className={css.radioContainer}>
+                  <input
+                    type="radio"
+                    id="woman"
+                    value="woman"
+                    {...register('gender')}
+                    defaultChecked
+                  />
+                  <label
+                    htmlFor="woman"
+                    className={`${css.genderLabel} ${css.text}`}
+                  >
+                    Woman
+                  </label>
+                  <input
+                    type="radio"
+                    id="man"
+                    value="man"
+                    {...register('gender')}
+                  />
+                  <label
+                    htmlFor="man"
+                    className={`${css.genderLabel} ${css.text}`}
+                  >
+                    Man
+                  </label>
+                  {errors.gender && (
+                    <p className={css.errorMessage}>{errors.gender.message}</p>
+                  )}
+                </div>
+              </div>
+              <div className={`${css.userDataWrapper} ${css.bigGap}`}>
                 <label
-                  htmlFor="woman"
-                  className={`${css.genderLabel} ${css.text}`}
+                  htmlFor="name"
+                  className={`${css.boldText} ${css.inputLabel}`}
                 >
-                  Woman
+                  Your name
                 </label>
                 <input
-                  type="radio"
-                  id="man"
-                  value="man"
-                  {...register('gender')}
+                  type="text"
+                  id="name"
+                  {...register('name')}
+                  onBlur={() => trigger('name')}
+                  className={`${css.inputs} ${css.smallGap}`}
                 />
+                {errors.name && (
+                  <p className={css.errorMessage}>{errors.name.message}</p>
+                )}
                 <label
-                  htmlFor="man"
-                  className={`${css.genderLabel} ${css.text}`}
+                  htmlFor="email"
+                  className={`${css.boldText} ${css.inputLabel}`}
                 >
-                  Man
+                  Email
                 </label>
-                {errors.gender && (
-                  <p className={css.errorMessage}>{errors.gender.message}</p>
+                <input
+                  type="email"
+                  id="email"
+                  {...register('email')}
+                  onBlur={() => trigger('email')}
+                  className={css.inputs}
+                />
+                {errors.email && (
+                  <p className={css.errorMessage}>{errors.email.message}</p>
+                )}
+              </div>
+              <div className={css.formulaContainer}>
+                <p className={`${css.boldText} ${css.smallGap}`}>
+                  My daily norma
+                </p>
+                <div className={`${css.formulaWrapper} ${css.smallGap}`}>
+                  <div className={css.formulaBlock}>
+                    <p className={`${css.text} ${css.miniGap}`}>For woman:</p>
+                    <p className={`${css.text} ${css.formula}`}>
+                      V=(M*0,03) + (T*0,4)
+                    </p>
+                  </div>
+                  <div className={css.formulaBlock}>
+                    <p className={`${css.text} ${css.miniGap}`}>For man:</p>
+                    <p className={`${css.text} ${css.formula}`}>
+                      V=(M*0,04) + (T*0,6)
+                    </p>
+                  </div>
+                </div>
+                <p className={`${css.spanText} ${css.text} ${css.smallGap}`}>
+                  <span className={css.specialSign}>*</span> V is the volume of
+                  the water norm in liters per day, M is your body weight, T is
+                  the time of active sports, or another type of activity
+                  commensurate in terms of loads (in the absence of these, you
+                  must set 0)
+                </p>
+                <span className={`${css.activeTimeWrapper} ${css.bigGap}`}>
+                  <svg className={css.iconExclamationMark}>
+                    <use href={`${sprite}#icon-exclamation-mark`}></use>
+                  </svg>
+                  <p className={css.text}>Active time in hours</p>
+                </span>
+              </div>
+            </div>
+
+            <div className={css.rightSide}>
+              <div className={css.waterWrapper}>
+                <label
+                  htmlFor="weight"
+                  className={`${css.inputLabel} ${css.text}`}
+                >
+                  Your weight in kilograms:
+                </label>
+                <input
+                  type="number"
+                  id="weight"
+                  {...register('weight')}
+                  onBlur={() => trigger('weight')}
+                  className={`${css.inputs} ${css.smallGap}`}
+                />
+                {errors.weight && (
+                  <p className={css.errorMessage}>{errors.weight.message}</p>
+                )}
+                <label
+                  htmlFor="sportTime"
+                  className={`${css.inputLabel} ${css.text}`}
+                >
+                  The time of active participation in sports:
+                </label>
+                <input
+                  type="number"
+                  id="sportTime"
+                  {...register('sportTime')}
+                  onBlur={() => trigger('sportTime')}
+                  className={`${css.inputs} ${css.bigGap}`}
+                />
+                {errors.sportTime && (
+                  <p className={css.errorMessage}>{errors.sportTime.message}</p>
+                )}
+              </div>
+              <div className={css.waterWrapper}>
+                <div className={`${css.litersWrapper} ${css.smallGap}`}>
+                  <p className={css.text}>
+                    The required amount of water in liters per day:
+                  </p>
+                  <p className={`${css.spanWaterAmount} ${css.text}`}>
+                    {/* required amount of water */}L
+                  </p>
+                </div>
+                <label
+                  htmlFor="waterIntake"
+                  className={`${css.boldText} ${css.inputLabel}`}
+                >
+                  Write down how much water you will drink:
+                </label>
+                <input
+                  type="number"
+                  id="waterIntake"
+                  {...register('waterIntake')}
+                  onBlur={() => trigger('waterIntake')}
+                  className={css.inputs}
+                />
+                {errors.waterIntake && (
+                  <p className={css.errorMessage}>
+                    {errors.waterIntake.message}
+                  </p>
                 )}
               </div>
             </div>
-            <div className={`${css.userDataWrapper} ${css.bigGap}`}>
-              <label
-                htmlFor="name"
-                className={`${css.boldText} ${css.inputLabel}`}
-              >
-                Your name
-              </label>
-              <input
-                type="text"
-                id="name"
-                {...register('name')}
-                onBlur={() => trigger('name')}
-                className={`${css.inputs} ${css.smallGap}`}
-              />
-              {errors.name && (
-                <p className={css.errorMessage}>{errors.name.message}</p>
-              )}
-              <label
-                htmlFor="email"
-                className={`${css.boldText} ${css.inputLabel}`}
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                {...register('email')}
-                onBlur={() => trigger('email')}
-                className={css.inputs}
-              />
-              {errors.email && (
-                <p className={css.errorMessage}>{errors.email.message}</p>
-              )}
-            </div>
-            <div className={css.formulaContainer}>
-              <p className={`${css.boldText} ${css.smallGap}`}>
-                My daily norma
-              </p>
-              <div className={`${css.formulaWrapper} ${css.smallGap}`}>
-                <div className={css.formulaBlock}>
-                  <p className={`${css.text} ${css.miniGap}`}>For woman:</p>
-                  <p className={`${css.text} ${css.formula}`}>
-                    V=(M*0,03) + (T*0,4)
-                  </p>
-                </div>
-                <div className={css.formulaBlock}>
-                  <p className={`${css.text} ${css.miniGap}`}>For man:</p>
-                  <p className={`${css.text} ${css.formula}`}>
-                    V=(M*0,04) + (T*0,6)
-                  </p>
-                </div>
-              </div>
-              <p className={`${css.spanText} ${css.text} ${css.smallGap}`}>
-                <span className={css.specialSign}>*</span> V is the volume of
-                the water norm in liters per day, M is your body weight, T is
-                the time of active sports, or another type of activity
-                commensurate in terms of loads (in the absence of these, you
-                must set 0)
-              </p>
-              <span className={`${css.activeTimeWrapper} ${css.bigGap}`}>
-                <svg className={css.iconExclamationMark}>
-                  <use href={`${sprite}#icon-exclamation-mark`}></use>
-                </svg>
-                <p className={css.text}>Active time in hours</p>
-              </span>
-            </div>
-            <div className={css.waterWrapper}>
-              <label
-                htmlFor="weight"
-                className={`${css.inputLabel} ${css.text}`}
-              >
-                Your weight in kilograms:
-              </label>
-              <input
-                type="number"
-                id="weight"
-                {...register('weight')}
-                onBlur={() => trigger('weight')}
-                className={`${css.inputs} ${css.smallGap}`}
-              />
-              {errors.weight && (
-                <p className={css.errorMessage}>{errors.weight.message}</p>
-              )}
-              <label
-                htmlFor="sportTime"
-                className={`${css.inputLabel} ${css.text}`}
-              >
-                The time of active participation in sports:
-              </label>
-              <input
-                type="number"
-                id="sportTime"
-                {...register('sportTime')}
-                onBlur={() => trigger('sportTime')}
-                className={`${css.inputs} ${css.bigGap}`}
-              />
-              {errors.sportTime && (
-                <p className={css.errorMessage}>{errors.sportTime.message}</p>
-              )}
-            </div>
-            <div className={css.waterWrapper}>
-              <div className={`${css.litersWrapper} ${css.smallGap}`}>
-                <p className={css.text}>
-                  The required amount of water in liters per day:
-                </p>
-                <p className={`${css.spanWaterAmount} ${css.text}`}>
-                  {/* required amount of water */}L
-                </p>
-              </div>
-              <label
-                htmlFor="waterIntake"
-                className={`${css.boldText} ${css.inputLabel}`}
-              >
-                Write down how much water you will drink:
-              </label>
-              <input
-                type="number"
-                id="waterIntake"
-                {...register('waterIntake')}
-                onBlur={() => trigger('waterIntake')}
-                className={css.inputs}
-              />
-              {errors.waterIntake && (
-                <p className={css.errorMessage}>{errors.waterIntake.message}</p>
-              )}
-            </div>
-            <button type="submit" className={css.button}>
-              Save
-            </button>
           </div>
+          <button type="submit" className={css.button}>
+            Save
+          </button>
         </form>
       </div>
     </div>
