a5ef72be62a600f9b4669c5f93d8378ee2413bfd
Oleksandr <gayrsuk@gmail.com>
2024-09-21 22:00:21 +0300

    Added basic settings for adding a translation

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import EN from './locales/en/translation.json';
+import UA from './locales/ua/translation.json';
+
+const resources = {
+  en: { translation: EN },
+  ua: { translation: UA },
+};
+
+const userLang = navigator.language.slice(0, 2);
+let lang = resources[userLang] ? userLang : 'en';
+
+i18n.use(initReactI18next).use(LanguageDetector).init({
+  resources,
+  lng: lang,
+  fallbackLng: 'en',
+  debug: false,
+});
+
+export const formatDate = (date) => {
+  return date.toLocaleDateString(lang === 'ua' ? 'uk-UA' : 'en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
+
+export default i18n;
