47c450fc7da854bbeb6ed54712f432dd0b9bb060
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-09-23 14:08:52 +0200

    update delete water

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { MODAL_NAME } from '../../constants';
-import css from './DeleteWaterModal.module.css'
+import css from './DeleteWaterModal.module.css';
 import { deleteWater } from '../../redux/water/operations';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
@@ -10,30 +10,29 @@ const DeleteWaterModal = ({ onClose, water, setWater }) => {
   const { t } = useTranslation();
   const [isDisabled, setIsDisabled] = useState(false);
   const dispatch = useDispatch();
-  
+
   const handleModalClose = () => {
     onClose(MODAL_NAME.DELETE_WATER_MODAL);
-  }
+  };
 
   const handleOnDelete = () => {
     setIsDisabled(true);
     dispatch(deleteWater(water))
       .unwrap()
       .then(() => {
-        toast.success(t('modal.success'), {duration: 2500});
+        toast.success(t('modal.success'), { duration: 800 });
         setTimeout(() => {
+          setIsDisabled(false);
           onClose(MODAL_NAME.DELETE_WATER_MODAL);
-        }, 2500);
+        }, 800);
         setWater(null);
       })
       .catch(() => {
+        setIsDisabled(false);
         toast.error(t('modal.error'), {
-          duration: 2000,
-        });       
-      })
-    .finally(() => {
-      setIsDisabled(false);
-    })
+          duration: 800,
+        });
+      });
   };
 
   return (
@@ -43,8 +42,19 @@ const DeleteWaterModal = ({ onClose, water, setWater }) => {
         <p>{t('modal.delete_confirmation')}</p>
       </div>
       <div className={css.deleteButtons}>
-        <button className={`${css.button} ${css.deleteBtn}`} onClick={handleOnDelete} disabled={isDisabled}>{t('modal.delete')}</button>
-        <button className={`${css.button} ${css.cancelBtn}`} onClick={handleModalClose}>{t('modal.cancel')}</button>
+        <button
+          className={`${css.button} ${css.deleteBtn}`}
+          onClick={handleOnDelete}
+          disabled={isDisabled}
+        >
+          {t('modal.delete')}
+        </button>
+        <button
+          className={`${css.button} ${css.cancelBtn}`}
+          onClick={handleModalClose}
+        >
+          {t('modal.cancel')}
+        </button>
       </div>
     </div>
   );
