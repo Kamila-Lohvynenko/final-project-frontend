c75320324c338cc389ff39a55f658e2b7096993e
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-09-23 21:28:39 +0200

    ok

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,29 @@
+// Logo.js
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import css from './Logo.module.css';
 
 const Logo = () => {
+  const { i18n } = useTranslation();
+  const [isEngLanguage, setIsEngLanguage] = useState(localStorage.getItem('i18nextLng') === 'en');
+
+  const toggleLanguage = () => {
+    const newLanguage = isEngLanguage ? 'ua' : 'en';
+    setIsEngLanguage(newLanguage === 'en');
+    localStorage.setItem('i18nextLng', newLanguage);
+    i18n.changeLanguage(newLanguage);
+  };
+
   return (
     <div className={css.logo_block}>
       <p className={css.logo}>AquaTrack</p>
+      <button 
+        className={css.languageSwitcher} 
+        onClick={toggleLanguage}
+      >
+        <span className={css.text}>{isEngLanguage ? 'En' : 'Ua'}</span>
+        <span className={css.arrow}>▼</span> {/* Стрелочка */}
+      </button>
     </div>
   );
 };
